52b80b5dfbc96f02ac315333e6b0c763ff32997b
IsiD12 <isabelldietrich12@web.de>
2021-03-12 10:36:20 +0100

    Update DeviantArt Eclipse - Extras.user.js
    Note button didnt work when browsing deviation. fixed

--- DeviantArt Eclipse - Extras.user.js
+++ DeviantArt Eclipse - Extras.user.js
@@ -5,7 +5,7 @@
 // @author       Isi-Daddy
 // @include      https://www.deviantart.com/
 // @include      https://www.deviantart.com/*
-// @version      1.1.11
+// @version      1.1.12
 // @grant GM_getValue
 // @require http://code.jquery.com/jquery-3.4.1.min.js
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
@@ -66,33 +66,55 @@ function klassentest (subj) {
 }
 
 
-
+//feedback
 var noteCount = '';
 if($("button._3ARPi._1jO0k").find("span").hasClass("_2g-3V")){
     noteCount = '<span class="_2g-3V">' + $("._3ykoX._32NlD._1DiCV span._2g-3V").text()+  '</span>' ;
 }
-//feedback
 $("button._3ARPi._1jO0k").remove();
 $("div._3ykoX._32NlD._1DiCV").append('<a class="_3ARPi _1jO0k" href="https://www.deviantart.com/notifications/notes/"><span><span class="_1MJER _3N3is _3wBuq"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M19.914 4a1 1 0 011 1v12.586a1 1 0 01-.293.707l-1.414 1.414A1 1 0 0118.5 20h-15a.5.5 0 01-.5-.5 1 1 0 01.4-.8L5 17.5l.02-11.089a1 1 0 01.29-.703l1.396-1.411A1 1 0 017.417 4h12.497zM9 11a1 1 0 100 2 1 1 0 000-2zm4 0a1 1 0 100 2 1 1 0 000-2zm4 0a1 1 0 100 2 1 1 0 000-2z"></path></svg></span>'+noteCount+'<span class="_2IZ0h">Chat</span></span></a>');
 
 
+//chat
 var noteCountChat = '';
 if($("button._1W4CQ._29ksA").find("span").hasClass("_1XPtL")){
     noteCountChat = '<span class="_1XPtL">' + $("._3Po3V._3-z6A.Ha0AC span._1XPtL").text()+  '</span>' ;
 }
-//chat
 $("button._1W4CQ._29ksA").remove();
 $("div._3Po3V._3-z6A.Ha0AC").append('<a class="_1W4CQ _29ksA" href="https://www.deviantart.com/notifications/notes/"><span><span class="_2Ae1S _2JjnE _2fqBI"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M19.914 4a1 1 0 011 1v12.586a1 1 0 01-.293.707l-1.414 1.414A1 1 0 0118.5 20h-15a.5.5 0 01-.5-.5 1 1 0 01.4-.8L5 17.5l.02-11.089a1 1 0 01.29-.703l1.396-1.411A1 1 0 017.417 4h12.497zM9 11a1 1 0 100 2 1 1 0 000-2zm4 0a1 1 0 100 2 1 1 0 000-2zm4 0a1 1 0 100 2 1 1 0 000-2z"></path></svg></span>'+noteCountChat+'<span class="_1xfU3">Chat</span></span></a>');
 
+
+//profile
 var noteCountProfile = '';
 if($("button._2nRKR.rDcsd").find("span").hasClass("_1UHKo")){
     noteCountProfile = '<span class="_1UHKo">' + $("._3ZzBG._3_-De._2Bu5u span._1UHKo").text()+  '</span>' ;
 }
-//profile
 $("button._2nRKR.rDcsd").remove();
 $("div._3ZzBG._3_-De._2Bu5u").append('<a class=" _2nRKR rDcsd" href="https://www.deviantart.com/notifications/notes/"><span><span class="_3HH04 _3s0_v YU6Gg"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M19.914 4a1 1 0 011 1v12.586a1 1 0 01-.293.707l-1.414 1.414A1 1 0 0118.5 20h-15a.5.5 0 01-.5-.5 1 1 0 01.4-.8L5 17.5l.02-11.089a1 1 0 01.29-.703l1.396-1.411A1 1 0 017.417 4h12.497zM9 11a1 1 0 100 2 1 1 0 000-2zm4 0a1 1 0 100 2 1 1 0 000-2zm4 0a1 1 0 100 2 1 1 0 000-2z"></path></svg></span>'+noteCountProfile+'<span class="_3c9b0">Chat</span></span></a>');
+
+
+//pictures
+var noteCountPic = '';
+if($("button._2VRYx._2uHE2").find("span").hasClass("ytR2V")){
+    noteCountPic = '<span class="ytR2V">' + $(".Rfy8Q._2vTIN._3rpge span.ytR2V").text()+  '</span>' ;
+}
+$("button._2VRYx._2uHE2").remove();
+$("div.Rfy8Q._2vTIN._3rpge").append('<a class="_2VRYx _2uHE2" href="https://www.deviantart.com/notifications/notes/"><span><span class="_3HH04 _3s0_v YU6Gg"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M19.914 4a1 1 0 011 1v12.586a1 1 0 01-.293.707l-1.414 1.414A1 1 0 0118.5 20h-15a.5.5 0 01-.5-.5 1 1 0 01.4-.8L5 17.5l.02-11.089a1 1 0 01.29-.703l1.396-1.411A1 1 0 017.417 4h12.497zM9 11a1 1 0 100 2 1 1 0 000-2zm4 0a1 1 0 100 2 1 1 0 000-2zm4 0a1 1 0 100 2 1 1 0 000-2z" style="fill: currentColor;"></path></svg></span>'+noteCountPic+'<span class="_1fSuN">Chat</span></span></a>');
+
+
 
 
 $("button.ucOYB._1u2kK._1Q3Tp").remove();
 var user = top.location.pathname.substring(1)
 $("div.tdh7l").append('<a class="ucOYB _1u2kK _1Q3Tp" href="https://www.deviantart.com/notifications/notes/#to='+user.split('/')[0]+'">Note</a>');
+
+
+
+
+
+
+
+
+
+
+
